3fb3b1e580993ba91a7592132e1c8f09d970aade
Doedeledoedel <84157343+Diedeliedee@users.noreply.github.com>
2023-02-10 01:57:24 +0100

    Initialized Project

---
+++ Dungeon Generated/Assets/Scripts/Tools/Movement/Behaviors/Control.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Dodelie.Tools
+{
+    class Control : Behavior
+    {
+        private float m_inputRotation = 0f;
+
+        public Control(float inputRotation = 0f)
+        {
+            m_inputRotation = inputRotation;
+        }
+
+        public override Vector2 GetDesiredVelocity(Context context)
+        {
+            return Calc.RotateVector2(Controls.leftInput, m_inputRotation) * context.speed;
+        }
+    }
+}
---
+++ Dungeon Generated/Assets/Scripts/Tools/Movement/Controls.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Dodelie.Tools
+{
+    /// <summary>
+    /// Abstract class having minimal amount of necessary references to possible input.
+    /// </summary>
+    public class Controls
+    {
+        public static Vector2 leftInput { get; protected set; }
+        public static Vector2 rightInput { get; protected set; }
+
+        public static bool activeLeftInput { get => leftInput.sqrMagnitude > 0f; }
+        public static bool activeRightInput { get => rightInput.sqrMagnitude > 0f; }
+    }
+}
