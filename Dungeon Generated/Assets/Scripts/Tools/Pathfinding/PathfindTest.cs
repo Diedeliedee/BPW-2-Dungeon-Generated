7db9dade4d22b5a056ab02247c465f8981c57acf
Doedeledoedel <84157343+Diedeliedee@users.noreply.github.com>
2023-07-04 05:05:33 +0200

    Created pathfinding tool!!

---
+++ Dungeon Generated/Assets/Scripts/Tools/Pathfinding/PathfindTest.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using Joeri.Tools.Debugging;
+
+namespace Joeri.Tools
+{
+    public class PathfindTest : MonoBehaviour
+    {
+        [Header("Properties:")]
+        [SerializeField] private int m_gridExtents = 1;
+        [SerializeField] private Vector2Int[] m_allowedDirections = new Vector2Int[]
+        {
+            Vector2Int.up,
+            Vector2Int.down,
+            Vector2Int.left,
+            Vector2Int.right
+        };
+        [Space]
+        [SerializeField] [Range(0f, 1f)] private float m_pathLerp = 0f;
+
+        [Header("References:")]
+        [SerializeField] private Transform m_start;
+        [SerializeField] private Transform m_goal;
+        [Space]
+        [SerializeField] private GameObject m_lerpTest;
+
+        //  Run-time;
+        private Pathfinder m_pathFinder     = null;
+        private bool[,] m_validPositions    = null;
+
+        private Pathfinder.Result m_result  = null;
+
+        private void Start()
+        {
+            m_validPositions    = GetPositions();
+            m_pathFinder        = new Pathfinder(IsValidCoordinate, m_allowedDirections);
+            m_result            = m_pathFinder.GetPathResult(GetCoordinate(m_start.position), GetCoordinate(m_goal.position));
+
+            m_lerpTest.SetActive(true);
+            m_pathLerp = 0f;
+        }
+
+        private void Update()
+        {
+            if (m_result != null && m_result.path != null)
+            {
+                m_lerpTest.transform.position = GetWorldPosition(m_result.path.Lerp(m_pathLerp));
+            }
+        }
+
+        private bool[,] GetPositions(System.Action<Vector2Int, bool> onEvaluate = null)
+        {
+            var positions = new bool[m_gridExtents * 2, m_gridExtents * 2];
+
+            for (int x = 0; x < positions.GetLength(0); x++)
+            {
+                for (int y = 0; y < positions.GetLength(1); y++)
+                {
+                    var pos         = GetWorldPosition(new Vector2Int(x, y));
+                    var size        = Vector3.one;
+                    var occupied    = Physics.CheckBox(pos, size * 0.5f);
+
+                    positions[x, y] = !occupied;
+                    onEvaluate?.Invoke(new Vector2Int(x, y), occupied);
+                }
+            }
+            return positions;
+        }
+        private bool IsValidCoordinate(Vector2Int coordinate)
+        {
+            var gridSize    = m_gridExtents * 2;
+            var inXBounds   = coordinate.x >= 0 && coordinate.x < gridSize;
+            var inYBounds   = coordinate.y >= 0 && coordinate.y < gridSize;
+
+            if (!inXBounds || !inYBounds) return false;
+            return m_validPositions[coordinate.x, coordinate.y];
+        }
+
+        private Vector3 GetWorldPosition(Vector2 coordinates)
+        {
+            var localPosition   = new Vector3(coordinates.x - m_gridExtents + 0.5f, 0f, coordinates.y - m_gridExtents + 0.5f);
+            var worldPosition   = transform.position + localPosition;
+
+            return worldPosition;
+        }
+
+        private Vector2Int GetCoordinate(Vector3 worldPos)
+        {
+            var localPosition   = worldPos - transform.position;
+            var localCoordinate = new Vector2Int(Mathf.FloorToInt(localPosition.x) + m_gridExtents, Mathf.FloorToInt(localPosition.z) + m_gridExtents);
+
+            return localCoordinate;
+        }
+
+        private Color GetNodeColor(Vector2Int coordinate, bool occupied)
+        {
+            if (m_start != null && GetCoordinate(m_start.position) == coordinate)   return Color.yellow;
+            if (m_goal != null && GetCoordinate(m_goal.position) == coordinate)     return Color.yellow;
+            if (m_result != null)
+            {
+                if (m_result.path != null && m_result.path.Has(coordinate))         return new Color(0f, 0.5f, 1f);
+                if (m_result.openNodes.Contains(coordinate))                        return Color.green;
+                if (m_result.closedNodes.Contains(coordinate))                      return Color.red;
+            }
+            if (!occupied)                                                          return Color.white;
+                                                                                    return Color.black;
+        }
+
+        private void OnDrawGizmos()
+        {
+            void OnEvaluate(Vector2Int coordinates, bool occupied)
+            {
+                GizmoTools.DrawOutlinedBox(GetWorldPosition(coordinates), Vector3.one * 0.9f, GetNodeColor(coordinates, occupied), 0.5f, true, 0.5f);
+            }
+
+            GetPositions(OnEvaluate);
+        }
+    }
+}
---
+++ Dungeon Generated/Assets/Scripts/Tools/Pathfinding/Pathfinder.cs
@@ -0,0 +1,266 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Joeri.Tools
+{
+    /// <summary>
+    /// Simple A* pathfinder
+    /// </summary>
+    public class Pathfinder
+    {
+        private readonly ValidNodeCheck m_nodeCheck     = null;
+        private readonly Vector2Int[] m_validDirections = null;
+
+        public Pathfinder(ValidNodeCheck nodeCheck, Vector2Int[] validDirections)
+        {
+            m_nodeCheck         = nodeCheck;
+            m_validDirections   = validDirections;
+        }
+
+        public Path FindPath(Vector2Int start, Vector2Int goal)
+        {
+            return GetPathResult(start, goal).path;
+        }
+
+        public Result GetPathResult(Vector2Int start, Vector2Int goal)
+        {
+            var openNodes   = new Dictionary<Vector2Int, Node>();
+            var closedNodes = new Dictionary<Vector2Int, Node>();
+
+            var currentNode = new Node(start, goal);
+
+            /// Adds the passed in node to the open Nodes dictionary.
+            void AddNodeToOpen(Node node)
+            {
+                openNodes.Add(node.coordinates, node);
+            }
+
+            /// Checks if the passed in coordinates have a Node in the open dictionary, and switches the Node to the closed dictionary.
+            void SwitchNodeToClosed(Node node)
+            {
+                openNodes   .Remove(node.coordinates);
+                closedNodes .Add(node.coordinates, node);
+            }
+
+            /// Returns the Node with the lowest f-cost in the open Nodes dictionary.
+            Node FindLowestFCost()
+            {
+                Node lowest = openNodes.First().Value;
+
+                foreach (var node in openNodes)
+                {
+                    var lowerFCost = node.Value.fCost < lowest.fCost;
+                    var lowerHCost = node.Value.fCost == lowest.fCost && node.Value.hCost < lowest.hCost;
+
+                    if (!lowerFCost && !lowerHCost) continue;
+                    lowest = node.Value;
+                }
+                return lowest;
+            }
+
+            /// Returns a dictionary of all the neighboring coordinates of the current Node.
+            List<Vector2Int> GetNeighboringCoordinates()
+            {
+                var coordinates = new List<Vector2Int>();
+
+                for (int i = 0; i < m_validDirections.Length; i++)
+                {
+                    coordinates.Add(currentNode.coordinates + m_validDirections[i]);
+                }
+                return coordinates;
+            }
+
+            /// Returns a Path from the start to the current node.
+            Path RetraceNodes()
+            {
+                var coordinatesBundle   = new List<Vector2Int>();
+                var crumb               = currentNode;
+
+                while (true)
+                {
+                    coordinatesBundle.Add(crumb.coordinates);
+                    crumb = crumb.parent;
+                    if (crumb.coordinates == start)
+                    {
+                        coordinatesBundle.Add(crumb.coordinates);
+                        break;
+                    }
+                }
+                coordinatesBundle.Reverse();
+
+                return new Path(coordinatesBundle.ToArray());
+            }
+
+            AddNodeToOpen(currentNode);
+            while (true)
+            {
+                //  Exit the loop if there is no open path to the goal.
+                if (openNodes.Count <= 0)
+                {
+                    Debug.Log($"Pathfinding failed with zero remaining open nodes. {closedNodes.Count} nodes have been closed in the process.");
+                    return new Result(null, openNodes, closedNodes);
+                }
+
+                //  Find the Node with the lowest f-cost, and switch it to the closed Nodes dictionary.
+                currentNode = FindLowestFCost();
+                SwitchNodeToClosed(currentNode);
+
+                //  Return out of the loop if we reached the goal.
+                if (currentNode.coordinates == goal)
+                {
+                    return new Result(RetraceNodes(), openNodes, closedNodes);
+                }
+
+                //  Check every valid neighbor in the surroundings of the current node, and add the qualified nodes to the open dictionary.
+                foreach (var coordinates in GetNeighboringCoordinates())
+                {
+                    //  If the neighbor's coordinates cannot bear a valid node, or already bears a node in the closed dictionary, skip the iteration.
+                    if (!IsValidNode(coordinates) || closedNodes.ContainsKey(coordinates)) continue;
+
+                    var newNeighbor = new Node(coordinates, goal, currentNode);
+
+                    //  If a node already exists in with the neighboring coordinates..
+                    if (openNodes.TryGetValue(coordinates, out Node neighboringNode))
+                    {
+                        //  Skip if the path through the newly created node is longer than the existing neighboring node.
+                        if (newNeighbor.gCost >= neighboringNode.gCost) continue;
+
+                        //  Otherwise, remove the existing node first, and..
+                        openNodes.Remove(coordinates);
+                    }
+
+                    //  Add the newly created node in the open dictionary.
+                    AddNodeToOpen(newNeighbor);
+                }
+            }
+        }
+
+        /// <returns>True if the passed in coordinates could harbor a valid Node. False if not.</returns>
+        private bool IsValidNode(Vector2Int coordinates)
+        {
+            if (!m_nodeCheck(coordinates))  return false;
+                                            return true;
+        }
+
+        public class Result
+        {
+            public readonly Path path                       = null;
+            public readonly List<Vector2Int> openNodes      = null;
+            public readonly List<Vector2Int> closedNodes    = null;
+
+            public Result(Path path, Dictionary<Vector2Int, Node> openNodes, Dictionary<Vector2Int, Node> closedNodes)
+            {
+                this.path           = path;
+                this.openNodes      = openNodes.Keys.ToList();
+                this.closedNodes    = closedNodes.Keys.ToList();
+            }
+        }
+
+        public class Path
+        {
+            public readonly PathSlice[] path    = null;
+            public readonly float length        = 0;
+
+            public Vector2Int first
+            {
+                get => path[0].coordinates;
+            }
+
+            public Vector2Int last
+            {
+                get
+                {
+                    var lastSlice = path[path.Length - 1];
+
+                    return lastSlice.coordinates + lastSlice.offset;
+                }
+            }
+
+            public Path(params Vector2Int[] coordinatesBundle)
+            {
+                path = new PathSlice[coordinatesBundle.Length - 1];
+
+                for (int i = 0; i < coordinatesBundle.Length - 1; i++)
+                {
+                    path[i] = new PathSlice(coordinatesBundle[i], coordinatesBundle[i + 1], length);
+                    length += path[i].length;
+                }
+            }
+
+            public bool Has(Vector2Int coordinates)
+            {
+                for (int i = 0; i < path.Length; i++)
+                {
+                    if (path[i].coordinates == coordinates) return true;
+                }
+                return false;
+            }
+
+            public Vector2 Lerp(float t)
+            {
+                var desiredSlice = path[path.Length - 1];
+
+                t *= length;
+                foreach (var slice in path)
+                {
+                    if (slice.tail + slice.length < t) continue;
+                    desiredSlice = slice;
+                    break;
+                }
+
+                t -= desiredSlice.tail;     //  Cut off the excess tail length;
+                t /= desiredSlice.length;   //  Convert to percentage;
+
+                return desiredSlice.Lerp(t);
+            }
+
+            public class PathSlice
+            {
+                public readonly Vector2Int coordinates;
+                public readonly Vector2Int offset;
+
+                public readonly float length    = 0f;
+                public readonly float tail      = 0f;
+
+                public PathSlice(Vector2Int origin, Vector2Int next, float tail)
+                {
+                    coordinates = origin;
+                    offset      = next - origin;
+
+                    length      = offset.magnitude;
+                    this.tail   = tail;
+                }
+
+                public Vector2 Lerp(float t)
+                {
+                    return Vector2.Lerp(coordinates, coordinates + offset, t);
+                }
+            }
+        }
+
+        public class Node
+        {
+            public readonly Node parent = null;
+            public readonly Vector2Int coordinates;
+
+            public readonly int gCost = 0;
+            public readonly int hCost = 0;
+            public readonly int fCost = 0;
+
+            public Node(Vector2Int coordinates, Vector2Int goal, Node parent = null)
+            {
+                this.parent         = parent;
+                this.coordinates    = coordinates;
+
+                if (parent != null) gCost = parent.gCost + Mathf.RoundToInt((coordinates - parent.coordinates).magnitude * 10);
+                else                gCost = 0;
+
+                hCost = Mathf.RoundToInt((goal - coordinates).magnitude * 10);
+                fCost = gCost + hCost;
+            }
+        }
+    
+        public delegate bool ValidNodeCheck(Vector2Int coordinates);
+    }
+}
