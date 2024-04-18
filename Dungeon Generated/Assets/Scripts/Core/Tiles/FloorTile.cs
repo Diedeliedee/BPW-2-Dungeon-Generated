573d3e043eefd4df71888d78675f99fd3493cb99
Joeri van Alphen <jralphen@kpnmail.nl>
2024-04-18 14:34:43 +0200

    Created new project

--- Dungeon Generated/Assets/Scripts/Core/Tiles/FloorTile.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class FloorTile : Tile
-{
-    public FloorTile(Room parent, Vector2Int coords) : base(parent, coords) { }
-}
--- Dungeon Generated/Assets/Scripts/Core/Tiles/WallTile.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class WallTile : Tile
-{
-    public WallTile(Room parent, Vector2Int coords) : base(parent, coords) { }
-}
